8078a3d01b10741dc65dd9064a18596298d54fe2
BirdPersonFromDorm <rmelinevskiy@gmail.com>
2022-03-26 23:28:08 +0300

    init commit

---
+++ src/components/Categories.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+function Categories({items}) {
+
+    const [activeItem, setActiveItem] = useState(null);
+
+    const onSelectedItem = (index) =>{
+        setActiveItem(index);
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                <li className={activeItem === null ? "active" : "" }
+                    onClick={() => (onSelectedItem(null))}>Все</li>
+                {
+                    items.map((name, index) =>
+                        <li className={activeItem === index ? "active" : "" }
+                            onClick={() => onSelectedItem(index)} key={`${name}_${index}`}>{name}</li>)
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default Categories;
